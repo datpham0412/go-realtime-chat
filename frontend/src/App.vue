a3e54bb119026dd65d18facfee64b641c5432a03
datpham0412 <tiendat041202@gmail.com>
2024-11-11 15:04:13 +1100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1 class="mt-5 mb-4">GraphQL Chat Test Github Workflow</h1>
+    <h1 class="mt-5 mb-4">GraphQL Chat Test Token</h1>
     <router-view/>
   </div>
 </template>
