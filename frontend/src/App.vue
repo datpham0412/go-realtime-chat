af5bada84874ffcf891bbe9d106b739f35fe5d47
datpham0412 <tiendat041202@gmail.com>
2024-11-14 10:05:24 +1100

    feat: update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="container">
-    <h1 class="mt-5 mb-4">Go Realtime Chat by Dat Pham</h1>
-    <router-view/>
+  <div id="app">
+    <header>
+      <GitHubLogin />
+    </header>
+    <main>
+      <ChatRoom />
+    </main>
   </div>
 </template>
 
 <script>
-export default {};
+import GitHubLogin from './components/GitHubLogin.vue'
+import ChatRoom from './components/ChatRoom.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GitHubLogin,
+    ChatRoom
+  }
+}
 </script>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+
+header {
+  margin-bottom: 2rem;
+}
+</style>
